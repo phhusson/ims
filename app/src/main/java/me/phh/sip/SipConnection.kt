62e98b712ae572ce67fe28dc1038dc9ab9a17be6
Pierre-Hugues Husson <phh@phh.me>
2025-01-11 01:43:07 +0100

    Make a generic SipConnection type, so that main connection can be either over tcp or udp -- LG U+ uses only UDP control channel

--- app/src/main/java/me/phh/sip/SipConnection.kt
+++ app/src/main/java/me/phh/sip/SipConnection.kt
@@ -5,7 +5,9 @@ import android.net.IpSecManager
 import android.net.IpSecTransform
 import android.net.Network
 import java.io.FileDescriptor
+import java.io.InputStream
 import java.io.OutputStream
+import java.net.DatagramPacket
 import java.net.DatagramSocket
 import java.net.InetAddress
 import java.net.InetSocketAddress
@@ -13,13 +15,26 @@ import java.net.ServerSocket
 import java.net.Socket
 
 /* wrapper around sockets + establish ipsec tunnel given ipsec helpers */
-
+interface SipConnection {
+    fun close()
+    fun enableIpsec(
+        ipSecBuilder: IpSecTransform.Builder,
+        ipSecManager: IpSecManager,
+        clientSpiC: IpSecManager.SecurityParameterIndex,
+        serverSpiS: IpSecManager.SecurityParameterIndex
+    )
+    fun gLocalAddr(): InetAddress
+    fun connect(remotePort: Int)
+    fun gWriter(): OutputStream
+    fun gReader(): SipReader
+    fun gLocalPort(): Int
+}
 class SipConnectionTcp(
     val network: Network,
     val remoteAddr: InetAddress,
     val _localAddr: InetAddress? = null,
     val _localPort: Int = 0
-) {
+) : SipConnection {
     val socket: Socket
     /* redefine public localAddr/port for when not specified in argument */
     var localAddr: InetAddress
@@ -42,7 +57,7 @@ class SipConnectionTcp(
         localPort = socket.localPort
     }
 
-    fun connect(_remotePort: Int) {
+    override fun connect(_remotePort: Int) {
         remotePort = _remotePort
         socket.connect(InetSocketAddress(remoteAddr, remotePort))
         if (_localAddr == null) {
@@ -55,11 +70,23 @@ class SipConnectionTcp(
         connected = true
     }
 
-    fun close() {
+    override fun gWriter(): OutputStream {
+        return writer
+    }
+
+    override fun gReader(): SipReader {
+        return reader
+    }
+
+    override fun gLocalPort(): Int {
+        return localPort
+    }
+
+    override fun close() {
         socket.close()
     }
 
-    fun enableIpsec(
+    override fun enableIpsec(
         ipSecBuilder: IpSecTransform.Builder,
         ipSecManager: IpSecManager,
         clientSpiC: IpSecManager.SecurityParameterIndex,
@@ -71,6 +98,10 @@ class SipConnectionTcp(
         ipSecManager.applyTransportModeTransform(socket, IpSecManager.DIRECTION_IN, inTransform)
         outTransform = ipSecBuilder.buildTransportModeTransform(localAddr, serverSpiS)
         ipSecManager.applyTransportModeTransform(socket, IpSecManager.DIRECTION_OUT, outTransform)
+    }
+
+    override fun gLocalAddr(): InetAddress {
+        return localAddr
     }
 }
 
@@ -122,6 +153,117 @@ class SipConnectionTcpServer(
 class SipConnectionUdp(
     val network: Network,
     val remoteAddr: InetAddress,
+    val _localAddr: InetAddress? = null,
+    val _localPort: Int = 0,
+) : SipConnection {
+    val socket: DatagramSocket
+    /* redefine public localAddr/port for when not specified in argument */
+    var localAddr: InetAddress
+    var localPort: Int
+    var remotePort: Int = 0
+    lateinit var writer: OutputStream
+    lateinit var reader: SipReader
+    // we need to keep the transform around or the ipsec transform
+    // gets destroyed while still in use
+    lateinit var inTransform: IpSecTransform
+    lateinit var outTransform: IpSecTransform
+    var connected = false
+
+    init {
+        if (_localAddr != null) {
+            socket = DatagramSocket(_localPort, _localAddr)
+        } else {
+            socket = DatagramSocket()
+        }
+        network.bindSocket(socket)
+
+        localAddr = socket.localAddress
+        localPort = socket.localPort
+    }
+
+    override fun connect(_remotePort: Int) {
+        remotePort = _remotePort
+        // Note: DO NOT connect, because the answers might come back from a different IP than where we sent to
+        //socket.connect(InetSocketAddress(remoteAddr, remotePort))
+        if (_localAddr == null) {
+            // localAddr/Port only valid after connect if no explicit bind
+            localAddr = socket.localAddress
+            localPort = socket.localPort
+        }
+        writer = object: OutputStream() {
+            override fun write(p0: Int) {
+                socket.send(DatagramPacket(byteArrayOf(p0.toByte()), 1, remoteAddr, remotePort))
+            }
+            override fun write(p0: ByteArray) {
+                socket.send(DatagramPacket(p0, p0.size, remoteAddr, remotePort))
+            }
+        }
+        reader = object: InputStream() {
+            val currentDgram = DatagramPacket(ByteArray(1500), 1500)
+            var currentPosition = 0
+            var currentSize = 0
+            override fun read(): Int {
+                if (currentPosition >= currentSize) {
+                    socket.receive(currentDgram)
+                    currentPosition = 0
+                    currentSize = currentDgram.length
+                }
+                return currentDgram.data[currentPosition++].toInt()
+            }
+
+            override fun read(b: ByteArray, off: Int, len: Int): Int {
+                if (currentPosition >= currentSize) {
+                    socket.receive(currentDgram)
+                    currentPosition = 0
+                    currentSize = currentDgram.length
+                }
+                val toRead = minOf(len, currentSize - currentPosition)
+                currentDgram.data.copyInto(b, off, currentPosition, currentPosition + toRead)
+                currentPosition += toRead
+                return toRead
+            }
+        }.sipReader()
+        connected = true
+    }
+
+    override fun gWriter(): OutputStream {
+        return writer
+    }
+
+    override fun gReader(): SipReader {
+        return reader
+    }
+
+    override fun gLocalPort(): Int {
+        return localPort
+    }
+
+    override fun close() {
+        socket.close()
+    }
+
+    override fun enableIpsec(
+        ipSecBuilder: IpSecTransform.Builder,
+        ipSecManager: IpSecManager,
+        clientSpiC: IpSecManager.SecurityParameterIndex,
+        serverSpiS: IpSecManager.SecurityParameterIndex
+    ) {
+        // Can only do this before connecting?
+        check(!connected)
+        inTransform = ipSecBuilder.buildTransportModeTransform(remoteAddr, clientSpiC)
+        ipSecManager.applyTransportModeTransform(socket, IpSecManager.DIRECTION_IN, inTransform)
+        outTransform = ipSecBuilder.buildTransportModeTransform(localAddr, serverSpiS)
+        ipSecManager.applyTransportModeTransform(socket, IpSecManager.DIRECTION_OUT, outTransform)
+    }
+
+    override fun gLocalAddr(): InetAddress {
+        return localAddr
+    }
+}
+
+class SipConnectionUdpServer(
+    val network: Network,
+    val remoteAddr: InetAddress,
     val localAddr: InetAddress,
     val localPort: Int) {
 
@@ -135,6 +277,34 @@ class SipConnectionUdp(
         socketFd =
             socket.javaClass.getMethod("getFileDescriptor\$").invoke(socket)
                 as FileDescriptor
+    }
+
+    fun gReader(): SipReader {
+        return object: InputStream() {
+            val currentDgram = DatagramPacket(ByteArray(1500), 1500)
+            var currentPosition = 0
+            var currentSize = 0
+            override fun read(): Int {
+                if (currentPosition >= currentSize) {
+                    socket.receive(currentDgram)
+                    currentPosition = 0
+                    currentSize = currentDgram.length
+                }
+                return currentDgram.data[currentPosition++].toInt()
+            }
+
+            override fun read(b: ByteArray, off: Int, len: Int): Int {
+                if (currentPosition >= currentSize) {
+                    socket.receive(currentDgram)
+                    currentPosition = 0
+                    currentSize = currentDgram.length
+                }
+                val toRead = minOf(len, currentSize - currentPosition)
+                currentDgram.data.copyInto(b, off, currentPosition, currentPosition + toRead)
+                currentPosition += toRead
+                return toRead
+            }
+        }.sipReader()
     }
 
     fun enableIpsec(
